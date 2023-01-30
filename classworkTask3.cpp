6f63b80437b55e9e510be415ddc52775bc93cd35
annafilyakina <91047140+annafilyakina@users.noreply.github.com>
2023-01-30 10:26:15 +0300

    Add files via upload

---
+++ classworkTask3.cpp
@@ -0,0 +1,52 @@
+﻿#include <stdlib.h>
+#include <iostream>
+#include <time.h>
+void __cout (int** array, int lines, int columns) {
+    
+    for (int i = 0; i < lines; i++)
+    {
+        std::cout << std::endl;
+        for (int j = 0; j < columns; j++) {
+            std::cout << array[i][j] << " ";
+
+        }
+    }
+}
+void __filling(int** array, int lines, int columns) {
+    srand(time(NULL));
+    for (int i = 0; i < lines; i++)
+    {
+        for (int j = 0; j < columns; j++) {
+
+            array[i][j] = 1 + rand() % 100;
+        }
+    }
+}
+
+int main()
+{
+    
+
+    std::cout << "enter the lines of your array " << std::endl;
+    int lines = 0;
+    std::cin >> lines;
+
+    std::cout << "enter the columns of your array " << std::endl;
+    int columns = 0;
+    std::cin >> columns;
+
+    int** array = new int* [lines];
+    for (int i = 0; i < lines; i++) {
+        array[i] = new int[columns];
+    }
+
+    int** ptr = array;
+    __filling(ptr, lines, columns);
+
+    
+    std::cout << "your array : " << std::endl;
+  
+    
+    __cout(ptr, lines, columns);
+}
+
