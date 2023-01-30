ec310bb32054115b7ce9a7ce611330eb3f3bdf42
annafilyakina <91047140+annafilyakina@users.noreply.github.com>
2023-01-30 11:20:16 +0300

    Add files via upload

---
+++ classworkTask4.cpp
@@ -0,0 +1,50 @@
+﻿
+#include <iostream>
+int min(int a,int b) {
+    if (a < b) {
+        return a;
+    }
+    else {
+        return b;
+    }
+}
+
+
+int main()
+{
+
+
+    std::cout << std::endl << "enter the length of your array " << std::endl;
+    int size = 0;
+    std::cin >> size;
+    int* array = new int[size]();
+
+    std::cout << std::endl << "enter the numbers " << std::endl;
+    for (int i = 0; i < size; i++) {
+        std::cin >> array[i];
+    }
+
+    std::cout << std::endl << "enter k " << std::endl;
+    int k = 0;
+    std::cin >> k;
+
+
+   
+    int s = 0;
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size - i - 1; j++) {
+            if (array[j] < array[j + 1]) { 
+                int t = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = t; 
+            }
+        }
+    }
+    for (int i = 0; i < k; i++) {
+        s += array[i];
+
+
+    }
+    int min_sredneye = s / k;
+    std::cout << min_sredneye;
+}
